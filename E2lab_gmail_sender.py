befb3f7782f01aca7c694dece1c3b0535a1dd69a
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-01-01 11:11:07 -0500

    Envio de mail en una fecha Fixed & creación de clase Participant

--- E2lab_gmail_sender.py
+++ E2lab_gmail_sender.py
@@ -4,8 +4,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime
-from datetime import timedelta
-import time
 
 CLIENT_SECRET_FILE = 'client_secret_2.json'
 API_NAME = 'gmail'
@@ -14,42 +12,46 @@ SCOPES = ['https://mail.google.com/']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 sched = BlockingScheduler()
-#mensaje que se desea enviar 
-def E2lab_Message(email):
 
-    message = '''
+class Participant():
+
+    def __init__(self,apellidos,nombre,email,fecha):
+        self.apellidos = apellidos
+        self.nombre = nombre
+        self.email = email
+        self.fecha = fecha 
+
+    def message(self,message):
+
+        message = '''
                 <!DOCTYPE html>
                 <html>
                     <body>
-                        <h1 style="color:SlateGray;">Hola Mundo! Te escribimos del E2labUP</h1>
+                        <h1 style="color:SlateGray;">Hola {} {}, te escribimos del E2labUP</h1>
                     </body>
                 </html>
-            '''  
+            '''.format(self.nombre,self.apellidos)  
 
-    mimeMessage1 = MIMEMultipart()
-    mimeMessage1['to'] = email # Destinatario (Cuentas de correos)
-    mimeMessage1['subject'] = 'Mensaje Prueba del E2labUP'
-    mimeMessage1.attach(MIMEText(message, 'html'))
-    raw_string1 = base64.urlsafe_b64encode(mimeMessage1.as_bytes()).decode()
+        mimeMessage1 = MIMEMultipart()
+        mimeMessage1['to'] = self.email # Destinatario (Cuentas de correos)
+        mimeMessage1['subject'] = 'Mensaje Prueba del E2labUP'
+        mimeMessage1.attach(MIMEText(message, 'html'))
+        raw_string1 = base64.urlsafe_b64encode(mimeMessage1.as_bytes()).decode()
 
-    message1 = service.users().messages().send(userId='me', body={'raw': raw_string1}).execute()
-    
-    global running
-    running = False
+        message1 = service.users().messages().send(userId='me', body={'raw': raw_string1}).execute()
+        
+        global running
+        running = False
 
-    print(message1)
-    print("El correo de prueba acaba de ser enviado")
-    
-#Formato de la fecha (ingresar como string): dd/mm/aaaa-hh:mm
-def E2lab_send_message(email,fecha):
+        print(message1)
+        print("El correo de prueba para {} {} acaba de ser enviado al {} ".format(self.nombre,self.apellidos,self.email))
+       
+    def send_email(self):
 
-    fecha = datetime.strptime(fecha, '%d/%m/%Y-%H:%M')
-    #
-    # Codigo que retiene el correo por un determinado tiempo
-    # para de ahi mandarlo
-    print("Mail para: "+email+" y llegará el: "+str(fecha))
-    E2lab_Message(email) #manda el mail
-    
+        self.fecha = datetime.strptime(self.fecha, '%d/%m/%Y-%H:%M')
+        sched.add_job(self.message, 'date', run_date=((self.fecha)), args=['text'])
+        print("El correo de prueba será enviado para {} {} al {} en {}".format(self.nombre,self.apellidos,self.email,self.fecha))
+        sched.start()
 
-#testeando el envio del mensaje: 
-E2lab_send_message('sg.mejiar@alum.up.edu.pe',"1/01/2022-10:05")
+participante = Participant('Mejia Ramos','Sergio Gonzalo','sg.mejiar@alum.up.edu.pe',"1/01/2022-11:02")
+participante.send_email()
